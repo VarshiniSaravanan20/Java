0bcd190b6f321950b51311f47c6caf0f11f3a464
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-07-27 23:05:38 +0530

    Add files via upload

---
+++ Decimaltooctal.java
@@ -0,0 +1,23 @@
+package practice_java;
+import java.util.Scanner;
+public class Decimaltooctal {
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        int octal = 0;
+        int place = 1;
+
+        while (decimal > 0) {
+            int rem = decimal % 8;     
+            octal = octal + (rem * place);
+            place = place * 10;
+            decimal = decimal / 8;
+        }
+
+        System.out.println("Octal = " + octal);
+    }
+}
