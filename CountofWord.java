ad933aaffeb38da01b6b7d19f0ce5b3c54cd8de4
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-08-07 21:58:31 +0530

    Add files via upload

---
+++ CountofWord.java
@@ -0,0 +1,19 @@
+package practice_java;
+import java.util.Scanner;
+public class CountofWord {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
