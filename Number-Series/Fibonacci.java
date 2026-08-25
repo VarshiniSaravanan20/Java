01d8080b8d12bfbbbdf85eaad8c331947b759e8d
Varshini Saravanan <siva.varshinisaravanan@gmail.com>
2026-08-25 13:55:52 +0530

    correction java

--- Number-Series/Fibonacci.java
+++ Number-Series/Fibonacci.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Fibonacci{
     public static void main(String[] args){
-        Scanenr sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a =0;
         int b=1;
         System.out.print(a+" "+b+" ");
-        for(int i=1;i<=n;++){
+        for(int i=1;i<=n;i++){
         int c=a+b;
         System.out.print(c+" ");
         a=b;
--- Number-Series/count.java
+++ Number-Series/count.java
@@ -10,3 +10,4 @@ public class count{
         }
         System.out.print(count);
     }
+}
